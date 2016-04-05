33ec37e8f7f39dc4e710f146b9c405ad9e04f19f
tadeu.classe <tadeu.classe@gmail.com>
2016-04-05 14:59:35 -0300

    Ionic Project

---
+++ ButucaLigadaIonic/ButucaLigada/www/scripts/index.js
@@ -0,0 +1,43 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
+// and then run "window.location.reload()" in the JavaScript Console.
+(function () {
+    "use strict";
+
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+
+    function onDeviceReady() {
+        // Handle the Cordova pause and resume events
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener('resume', onResume.bind(this), false);
+
+        var edtBusca = document.getElementById("edtBusca");
+
+        var btnTeste = document.getElementById("btnClick");
+        btnTeste.addEventListener('click', function() {
+
+            var cont = 0;
+
+            if (typeof(Storage) !== "undefined") {
+                if (localStorage.contador) {
+                    localStorage.contador = Number(localStorage.contador) + 1;
+                } else {
+                    localStorage.contador = 1;
+                }
+
+                cont = localStorage.contador;
+            }
+
+            edtBusca.value = cont.toString();
+        }, false);
+    };
+
+    function onPause() {
+        // TODO: This application has been suspended. Save application state here.
+    };
+
+    function onResume() {
+        // TODO: This application has been reactivated. Restore application state here.
+    };
+})();
