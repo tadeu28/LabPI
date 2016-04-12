957c2c9436b6f854bcd7e6d4a002d3733bec4105
tadeu.classe <tadeu.classe@gmail.com>
2016-04-12 17:05:11 -0300

    Registro GNU

--- FiscalDigital/ButucaLigada/www/scripts/index.js
+++ FiscalDigital/ButucaLigada/www/scripts/index.js
@@ -1,4 +1,26 @@
-﻿// For an introduction to the Blank template, see the following documentation:
+﻿/*
+    Fiscal Digital
+    Copyright (c) by Tadeu Classe, Leo Yukku, Luiza de Paula, Renata Araujo
+
+    https://github.com/tadeu28/LabPI/tree/prot_fase_I
+
+    This file is part of Fiscal Digital.
+
+    Fiscal Digital is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// For an introduction to the Blank template, see the following documentation:
 // http://go.microsoft.com/fwlink/?LinkID=397704
 // To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
 // and then run "window.location.reload()" in the JavaScript Console.
